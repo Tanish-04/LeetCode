3fa566dc4c74b1431cee16db2c32461813d9710e
Tanish-04 <63210891+Tanish-04@users.noreply.github.com>
2021-07-08 21:21:24 +0500

    Time: 144 ms (5.19%), Space: 15.1 MB (25.50%) - LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        
+        left = 0
+        right = len(A) - 1
+        
+        while left < right:
+            if (A[left] % 2) != 0:
+                A[left],A[right] = A[right],A[left]
+                right -= 1
+            else:
+                left += 1
+        
+        return A
+                    
+        
