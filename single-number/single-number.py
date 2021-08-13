3a60f39c15531716eddf679bc41352ace1b2f1bf
Tanish-04 <63210891+Tanish-04@users.noreply.github.com>
2021-08-13 15:48:33 +0500

    Time: 206 ms (22.16%), Space: 16.7 MB (59.50%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,15 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        dic = {}
+        result = 0
         
         for i in nums:
-            if i not in dic:
-                dic[i] = 1
-            else:
-                dic[i] += 1
+            result = result ^ i
             
-        for i in dic:
-            if dic[i] == 1:
-                return i
+        return result
         
