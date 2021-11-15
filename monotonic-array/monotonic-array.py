52724468df982ebb97ed1e5e62bd72637c40b6a1
Tanish-04 <63210891+Tanish-04@users.noreply.github.com>
2021-11-15 16:21:57 +0500

    Time: 1108 ms (29.91%), Space: 29.5 MB (8.91%) - LeetHub

--- monotonic-array/monotonic-array.py
+++ monotonic-array/monotonic-array.py
@@ -1,21 +1,7 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         
+        decreasingSeries = all([nums[i-1] >= nums[i] for i in range(1,len(nums))])
+        increasingSeries = all([nums[i-1] <= nums[i] for i in range(1,len(nums))])
         
-        isGreater = False
-        isLesser = False
-        
-        for i in range(1,len(nums)):
-            
-            if nums[i-1] > nums[i]:
-                isGreater = True
-            
-            if nums[i-1] < nums[i]:
-                isLesser = True
-                
-            
-            if isGreater and isLesser:
-                return False
-            
-        return True
-        
+        return decreasingSeries or increasingSeries
