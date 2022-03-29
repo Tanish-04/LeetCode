7c4b9f3d80a90d7b256914a9d35489e4d82ec290
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-03-29 14:57:50 +0500

    Time: 62 ms (32.52%), Space: 13.9 MB (66.34%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,7 +1,7 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         
-        # Approach # 1 Using Extra Space
+        # Approach Using Extra Space
         # Time Complexity O(n)
         # Space Complexity O(n)
         stack = []
@@ -16,14 +16,3 @@ class Solution:
         
         return "".join(stack)
     
-    
-#         while i < len(s)-1:
-#             if s[i] == s[i+1]:
-#                 i += 1
-#             elif s[i].lower() == s[i+1] or s[i].upper() == s[i+1]:
-#                 s = s[:i] + s[i+2:]
-#                 i = 0
-#             else:
-#                 i += 1
-        
-#         return s
