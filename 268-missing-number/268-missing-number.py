ba7d817e4f95392f8be6b0f9a66127803458d72b
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-05-02 19:38:05 +0500

    Time: 326 ms (16.40%), Space: 15.1 MB (98.80%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,16 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        # Time Complexity O(n)
-        # Space Complexity O(n)
         
-        dic = {}
+        # Using XOR Logic
+        
+        missingNumber = len(nums)
         
         for i in range(len(nums)):
-            dic[nums[i]] = i
             
-        for i in range(len(nums)+1):
-            if i not in dic:
-                return i
+            missingNumber ^= i
+            missingNumber ^= nums[i]
             
+        return missingNumber
+    
+        
+    
+    
+            
+        
         
