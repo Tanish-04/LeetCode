2ebf2f68c8055f032bae3bafac9595ac28f097c2
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-05-06 00:09:02 +0500

    Time: 49 ms (35.78%), Space: 14 MB (25.73%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,6 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
+        # Time Complexity: O(n)
+        # Space Complexity:O(n)
+        
         stack = []
         
         for i in s:
@@ -8,8 +11,13 @@ class Solution:
             if i == '(' or i == '[' or i == '{':
                 stack.append(i)
             
-            elif len(stack) == 0:
+            # This elif will run when string s starting with closing ')]}'
+            # so in that case valid parentheses condition will always be false
+            
+            elif len(stack) == 0: 
                 return False
+            # If forward parenthesis exist pop that element or return False
+            
             elif i == ')' and stack.pop() != '(':
                 return False
             
