3471a895ed2d686ed9bae4807e12ccb90dc1f5f4
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-05-08 01:39:07 +0500

    Time: 35 ms (70.71%), Space: 14 MB (5.70%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,6 +1,15 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
+        
+        # Time Complexity O(n)
+        # Space Complexity O(1)
+        '''
+        Iterate on array from the right and if space exist before any word
+        appear then decrement i and if any character occurs start counting
+        until it doesn't find space when iterating towards the left when
+        space found then return that wordlength
+        '''
         wordLength = 0
         
         for i in range(len(s)-1,-1,-1):
