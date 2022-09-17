f1f0d5422ef55883f224fd1991218717efc25800
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-09-17 15:00:18 +0500

    Time: 69 ms (32.51%), Space: 13.9 MB (32.54%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,46 +6,25 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        cur = dummy = ListNode()
-        while list1 and list2:               
-            if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+        '''result act as a pointer after comparing element of list1 and list2
+        and every element which point in the result will also add into dummy variable
+        a complete linked list will be generated into dummy node
+        '''
+        dummy = ListNode(-1)
+        result = dummy
+        
+        while list1 and list2:
+            
+            if list1.val <= list2.val:
+                result.next = list1
+                list1 = list1.next
             else:
-                cur.next = list2
-                list2, cur = list2.next, list2
+                result.next = list2
+                list2 = list2.next
+            result = result.next
                 
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
+        result.next = list1 if list1 != None else list2
+        
             
         return dummy.next
         
-        
-#         p1 = list1
-#         p2 = list2
-#         dummynode = ListNode(-1)
-#         p3 = dummynode
-#         while(p1 != None and p2 != None):
-            
-#             if p1.val < p2.val:
-#                 p3.next = p1
-#                 p1 = p1.next
-#             else:
-#                 p3.next = p2
-#                 p2 = p2.next
-#             print(p3)
-#             p3 = p3.next
-            
-#         while(p1 != None):
-#             p3.next = p1
-#             p1=p1.next
-#             p3=p3.next
-            
-            
-#         while(p2 != None):
-#             p3.next = p2
-#             p2=p2.next
-#             p3=p3.next
-            
-            
-#         return p3.next
