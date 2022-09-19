b0640f8fb7692a804e2fce8f5fc404a46b4158f3
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-09-19 19:54:28 +0500

    Time: 624 ms (8.38%), Space: 21.2 MB (81.95%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -8,25 +8,44 @@ class Solution:
         [24,12,4,1]
         Then multiply together
         '''
+        # Optimizing Space Complexity -> Approach 2
         n = len(nums)
-        left = [0] * n 
-        right = [0] * n
         answer = [0] * n
         
-        left[0] = 1
-        for i in range(1,len(nums)):
-            left[i] = left[i-1] * nums[i-1]
+        answer[0] = 1
+        for i in range(1,n):
+            answer[i] = answer[i-1] * nums[i-1]
         
-        right[n-1] = 1
+        right = 1
         
-        
-        for j in range(n-2,-1,-1):
-            right[j] = right[j+1]* nums[j+1]
-        
-        for i in range(n):
-            answer[i] = left[i] * right[i]
+        for j in range(n-1,-1,-1):
+            answer[j] = right * answer[j]
+            right *= nums[j]
             
         return answer
+        
+        # Approach 1
+#         n = len(nums)
+#         left = [0] * n 
+#         right = [0] * n
+#         answer = [0] * n
+        
+#         left[0] = 1
+#         for i in range(1,len(nums)):
+#             left[i] = left[i-1] * nums[i-1]
+        
+#         right[n-1] = 1
+        
+        
+#         for j in range(n-2,-1,-1):
+#             right[j] = right[j+1]* nums[j+1]
+        
+#         for i in range(n):
+#             answer[i] = left[i] * right[i]
+            
+#         return answer
+
+          
         
         
         
