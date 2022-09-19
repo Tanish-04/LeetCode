0289db78e76ad723eb768f7acb79174b105be98a
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-09-19 12:23:15 +0500

    Time: 2964 ms (5.01%), Space: 24.7 MB (96.59%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,34 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # kaden's algorithm pending
-        # Approach 1
-        '''
-        Time Complexity: O(n)
-        Space Compexity: O(1)
-        '''
-        left = 0 
+        
+        left = 0
         right = 1
-        maxProfit = 0
+        maximumProfit = 0
         
         while right < len(prices):
             if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
-                maxProfit = max(maxProfit,profit)
+                maximumProfit = max(profit, maximumProfit)
             else:
                 left = right
+            
             right += 1
-        return maxProfit
-    
-        
-        
-        
-        
-        
-        
-        
-    
-
-    
-        
+            
+        return maximumProfit
         
