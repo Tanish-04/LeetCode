75b6520f0dd473e233da34ace24e8fe27f5da037
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-09-21 19:18:18 +0500

    Time: 188 ms (23.39%), Space: 14 MB (93.03%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,12 +5,12 @@ class Solution:
         l = 0
         result = 0
         
-        for i in range(len(s)):
-            while s[i] in addSet:
+        for r in range(len(s)):
+            while s[r] in addSet:
                 addSet.remove(s[l])
                 l += 1
-            addSet.add(s[i])
-            result = max(result,i-l+1)
+            addSet.add(s[r])
+            result = max(result,r-l+1)
             
         return result
     
