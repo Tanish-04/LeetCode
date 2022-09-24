39b5284000a6653858f1f87defffbe91f1300088
Tanish_Kumar <63210891+Tanish-04@users.noreply.github.com>
2022-09-24 18:16:10 +0500

    Time: 2014 ms (5.00%), Space: 13.9 MB (55.49%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -2,19 +2,30 @@ class Solution:
     
     def fib(self, n: int) -> int:
         
-        firstNumber = 0
-        secondNumber = 1
+        # Recursion
         
-        if n == 1:
-            return secondNumber
         if n == 0:
-            return firstNumber
+            return 0
+        if n == 1:
+            return 1
         
-        result = 0
+        return self.fib(n-1) + self.fib(n-2)
         
-        for i in range(2,n+1):
-            result = firstNumber + secondNumber
-            firstNumber = secondNumber
-            secondNumber = result
+        
+        # Iterative 
+#         firstNumber = 0
+#         secondNumber = 1
+        
+#         if n == 1:
+#             return secondNumber
+#         if n == 0:
+#             return firstNumber
+        
+#         result = 0
+        
+#         for i in range(2,n+1):
+#             result = firstNumber + secondNumber
+#             firstNumber = secondNumber
+#             secondNumber = result
             
-        return result
+#         return result
